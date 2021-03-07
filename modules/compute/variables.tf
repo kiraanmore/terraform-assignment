8e2eace878b0f26d85f96410a07d070ab2e93f46
Kiran More <kiraan.more@hotmail.com>
2021-03-07 23:40:38 +0530

    Fixed variables

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -5,3 +5,39 @@ variable "instance_type" {
 variable "public_key_path" {
     type = string
 }
+
+variable "log-bucket-name" {
+    type = string
+}
+
+variable "vpc-id" {
+    type = string
+}
+
+variable "public-subnet-1-id" {
+    type = string
+}
+
+variable "public-subnet-2-id" {
+    type = string
+}
+
+variable "private-subnet-1-id" {
+    type = string
+}
+
+variable "private-subnet-2-id" {
+    type = string
+}
+
+variable "lb-sg-id" {
+    type = string
+}
+
+variable "webserver-sg-id" {
+    type = string
+}
+
+variable "bastion-sg-id" {
+    type = string
+}
